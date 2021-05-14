512e59e9a137c93f9a53952c3824ef6cfe088220
Kranthi <DharanikotaKranthi@JohnDeere.com>
2021-05-13 19:04:07 -0500

    initial commit

---
+++ src/main/java/com/git/explore/examples/HelloGitMain.java
@@ -0,0 +1,19 @@
+package com.git.explore.examples;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class HelloGitMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Git main !!");
+        new HelloGitMain().printMe();
+
+    }
+
+    private void printMe() {
+        Method[] declaredMethods = this.getClass().getDeclaredMethods();
+        Stream<Object> stream = Arrays.stream(Arrays.stream(declaredMethods).toArray());
+        stream.forEach(a -> System.out.println("method names are:::" + a));
+    }
+}
